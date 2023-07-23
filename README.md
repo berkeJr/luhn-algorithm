9b793c0d5f72b0bf7d70ff14dde5b21f2877da1c
Berke Sayın <sayinberkesayin@gmail.com>
2023-07-23 11:16:42 +0300

    Create function to validate credit card number

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # Kredi Kartı Numarası Validasyonu İçin Luhn Algoritması Kullanımı 
 
 
+[The Luhn Algorithm for Credit Card Validation](https://www.creditcardvalidator.org/articles/luhn-algorithm)
+
+Luhn Algoritması, kredi kartı numaralarının doğruluğunu kontrol etmek için kullanılan bir algoritmadır. 
+
+
+[Fake Credit Card Number Generator](https://www.creditcardvalidator.org/generator)
+
+Bu tool'u, seçilen kart sistemine bağlı olarak geçerli IIN'ler kullanan rastgele kredi kartı numaraları oluşturmak ve Luhn Algoritması validasyonunu test etmek için kullanabilirsiniz.
 
--- app.js
+++ app.js
@@ -1,3 +1,36 @@
-// Validation For Credit Card Numbers Using Luhn Algorithm 
+// Validation For Credit Card Numbers Using Luhn Algorithm
 
+const validateCreditCardNumber = (input) => {
+  // Kredi kartı numarasını sayısal bir diziye dönüştürme
+  let creditCardInt = input.split("").map(Number);
 
+  // Luhn Algoritması gereği her ikinci basamağı sondan başlayarak ikiyle çarparız
+  for (let i = creditCardInt.length - 2; i >= 0; i = i - 2) {
+    // Seçilen her ikinci basamağı geçici bir değişkende saklarız
+    let tempValue = creditCardInt[i];
+
+    // Seçilen basamağı ikiyle çarparız
+    tempValue = tempValue * 2;
+
+    /* Eğer ikiyle çarpma sonucu elde edilen değer 9'dan büyükse 
+        sonuç iki basamaklı bir sayı olacağından dolayı basamaklar toplanarak düzeltilir */
+    if (tempValue > 9) {
+      tempValue = (tempValue % 10) + 1;
+    }
+
+    // Düzeltme yapılan değerin orjinal dizideki ilgili basamağın üzerine yazılması
+    creditCardInt[i] = tempValue;
+  }
+
+  // Tüm basamakların toplamınının hesaplanması
+  let total = 0;
+  for (let i = 0; i < creditCardInt.length; i++) {
+    total += creditCardInt[i];
+  }
+
+  /* Toplamın 10'a bölümünden kalanı kontrol edilmesi ve kredi kartı numarasının geçerli 
+        olup olmadığının belirlenmesi */
+  return total % 10 === 0;
+};
+
+module.exports = validateCreditCardNumber;
